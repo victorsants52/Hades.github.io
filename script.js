e0e288165113d9ccb502d2f2e25c73cbc24c29a5
victorsants52 <benniezk56@gmail.com>
2026-08-24 02:48:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 function toggleMenu() {
-
     const menu = document.getElementById("mobileMenu");
 
     if (menu.style.display === "block") {
-
         menu.style.display = "none";
-
     } else {
-
         menu.style.display = "block";
-
     }
-
 }
