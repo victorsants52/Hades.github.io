1795356b35d3d824adefb43070cbcfa63fbddfc0
victorsants52 <benniezk56@gmail.com>
2026-08-24 02:44:25 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+
+    const menu = document.getElementById("mobileMenu");
+
+    if (menu.style.display === "block") {
+
+        menu.style.display = "none";
+
+    } else {
+
+        menu.style.display = "block";
+
+    }
+
+}
